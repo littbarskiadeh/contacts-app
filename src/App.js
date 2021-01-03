b8572192dad67ecb2fd99488f4c0c75e51ecddec
littbarski <lit.adeh@gmail.com>
2021-01-03 13:41:28 -0500

    added more files to initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Parent from './Components/Parent';
+import './Styles/App.css';
+
+function App() {
+
+  return (
+    <div className='main-container'>
+      <Parent />
+    </div>
+  );  
+}
+
+export default App;
